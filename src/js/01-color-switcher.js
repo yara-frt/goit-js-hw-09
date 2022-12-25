9526e1a7f7f0c84036695a5e80f138973567386c
Yaroslav Ashchaulov <y.ashchaulov@gmail.com>
2022-12-25 19:15:39 +0200

    added task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,39 @@
+const body = document.querySelector('body');
+const allBtn = {
+    startBtn: document.querySelectorAll('button')[0],
+    stopBtn: document.querySelectorAll('button')[1],
+}
 
-console.log('Hello world!')
+allBtn.startBtn.addEventListener('click', onClikStartBtn);
+allBtn.stopBtn.addEventListener('click', onClikStopBtn);
+
+onDisabled(allBtn.stopBtn);
+let intervalId = null;
+
+function onClikStartBtn() {
+    onDisabled(allBtn.startBtn);
+    closeDisabled(allBtn.stopBtn);
+    intervalId = setInterval(() => {
+        let newColor = getRandomHexColor();
+        console.log(newColor);
+        body.style.backgroundColor = newColor;
+    }, 1000)
+}
+
+function onClikStopBtn() {
+    closeDisabled(allBtn.startBtn);
+    onDisabled(allBtn.stopBtn);
+    clearInterval(intervalId);
+}
+
+function onDisabled(btn) {
+    btn.setAttribute('disabled', 'disabled');
+}
+
+function closeDisabled(btn) {
+    btn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
